a21125c40224e12586e096cff99cbe906f198dd4
aranubi <araceli.vera@nubiral.com>
2024-08-18 23:18:17 -0300

    update py version to 3.11

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-demo"
+    resource_group_name  = "rg-tfstate-arawebapp-demo"
     storage_account_name = "sawebappterraformstate"
     container_name       = "tfstatefile"
-    key                  = "stateActions.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
