45063920f313ffa608785a4b51805bbf67cc082d
aranubi <araceli.vera@nubiral.com>
2024-08-20 11:23:26 -0300

    update streamlit app

--- test-streamlit/app.py
+++ test-streamlit/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns 
 
-st.title("Sales KPI Dashboard by Ara")
+st.title("Sales KPI Dashboard by Ara v2")
 
 st.markdown('## Key Metrics')
 
